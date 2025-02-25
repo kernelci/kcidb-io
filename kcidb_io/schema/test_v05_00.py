a29e115ac3b28781d9d82090b320c82e8559aec2
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2025-02-25 12:43:36 +0200

    schema: v5.0: Remove 'contacts' from checkouts
    Remove the 'contacts' field from checkouts. It was added to support
    extracting email addresses from patches so that notifications could be
    sent to people concerned with them. Since then we've focused on
    subscription-based system to minimize negative effect of sending false
    positives/negatives to people who didn't ask for them.
    
    So this field is not needed now. That is unless we start getting perfect
    test results, which is not likely to ever happen.

--- kcidb_io/schema/test_v05_00.py
+++ kcidb_io/schema/test_v05_00.py
@@ -8,3 +8,37 @@ from kcidb_io.schema.v05_00 import Version
 def test_empty_upgrade():
     """Test upgrading an empty dataset"""
     assert Version.upgrade(Version.previous.new()) == Version.new()
+
+
+def test_contacts_removal():
+    """Check contacts field is being removed"""
+    old_data = dict(
+        **Version.previous.new(),
+        checkouts=[
+            dict(
+                id="origin:1", origin="origin",
+                contacts=["spbnick@gmail.com"],
+            ),
+            dict(
+                id="origin:2", origin="origin",
+                contacts=[],
+            ),
+        ],
+    )
+    new_data = dict(
+        **Version.new(),
+        checkouts=[
+            dict(
+                id="origin:1", origin="origin",
+            ),
+            dict(
+                id="origin:2", origin="origin",
+            ),
+        ],
+    )
+    invalid_upgraded_data = old_data.copy()
+    invalid_upgraded_data.update(**Version.new())
+    assert not Version.is_valid_exactly(invalid_upgraded_data)
+    assert Version.previous.is_valid_exactly(old_data)
+    assert Version.is_valid_exactly(new_data)
+    assert Version.upgrade(old_data) == new_data
