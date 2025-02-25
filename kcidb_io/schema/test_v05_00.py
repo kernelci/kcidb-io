3d4c59fd532964c786491a4240e7cf320f7a20ec
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2025-02-25 12:59:38 +0200

    schema: v5.0: Drop issue build_valid/test_status
    Drop the `build_valid` and `test_status` fields from `issues` in schema
    v5.0. The idea was to let submitters rectify results they submitted
    earlier, so that reports could be corrected. However, it seems that
    we're shifting our attention to notifying about "issues", rather than
    particular test and build results, so these fields wouldn't be needed,
    and their absence would make things simpler.

--- kcidb_io/schema/test_v05_00.py
+++ kcidb_io/schema/test_v05_00.py
@@ -42,3 +42,42 @@ def test_contacts_removal():
     assert Version.previous.is_valid_exactly(old_data)
     assert Version.is_valid_exactly(new_data)
     assert Version.upgrade(old_data) == new_data
+
+
+def test_build_valid_test_status_removal():
+    """Check build_valid and test_status fields are removed from issues"""
+    old_data1 = dict(**Version.previous.new(), issues=[
+        dict(
+            id="origin:1", origin="origin", version=1,
+            build_valid=True,
+        ),
+    ],)
+    old_data2 = dict(**Version.previous.new(), issues=[
+        dict(
+            id="origin:2", origin="origin", version=1,
+            test_status="PASS",
+        ),
+    ],)
+
+    new_data1 = dict(**Version.new(), issues=[
+        dict(id="origin:1", origin="origin", version=1,),
+    ],)
+    new_data2 = dict(**Version.new(), issues=[
+        dict(id="origin:2", origin="origin", version=1,),
+    ],)
+
+    assert Version.previous.is_valid_exactly(old_data1)
+    assert Version.is_valid_exactly(new_data1)
+    assert Version.upgrade(old_data1) == new_data1
+
+    assert Version.previous.is_valid_exactly(old_data2)
+    assert Version.is_valid_exactly(new_data2)
+    assert Version.upgrade(old_data2) == new_data2
+
+    invalid_upgraded_data1 = old_data1.copy()
+    invalid_upgraded_data1.update(**Version.new())
+    assert not Version.is_valid_exactly(invalid_upgraded_data1)
+
+    invalid_upgraded_data2 = old_data2.copy()
+    invalid_upgraded_data2.update(**Version.new())
+    assert not Version.is_valid_exactly(invalid_upgraded_data2)
